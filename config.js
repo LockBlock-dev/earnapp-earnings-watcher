9637c18e36d468831a433865c5cf906a75e003e0
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2021-11-14 12:23:11 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@ module.exports = {
 
     oauthRefreshToken: "COOKIE", //see https://github.com/LockBlock-dev/earnapp.js#how-to-login-with-cookies
 
-    modes: ["total", "perDevice"], //you can put one mode or both
+    modes: ["total", "perDevice", "transactions"], //you can put one mode or all of them
 
-    get_from_env: function () {
-        if (process.env.WEBHOOK_URL != undefined) {
+    get_from_env: () => {
+        if (process.env.WEBHOOK_URL) {
             this.discordWebhookURL = process.env.WEBHOOK_URL;
         }
-        if (process.env.AUTH != undefined) {
+        if (process.env.AUTH) {
             this.oauthRefreshToken = process.env.AUTH;
         }
-        if (process.env.MODE != undefined) {
-            let options = ["total", "perDevice", "both"]
+        if (process.env.MODE) {
+            let options = ["total", "perDevice", "transactions", "all"];
             if (options.includes(process.env.MODE)) {
                 this.modes = [process.env.MODE];
             }
